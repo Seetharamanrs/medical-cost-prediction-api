e92c25d52eb29d2bc7ff82d10f1db8c3207b69c1
seetharamanrs <seetharaman2632001@gmail.com>
2026-05-05 12:15:13 +0530

    API endppoint is created

--- api/api.py
+++ api/api.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 bas_dir=os.path.dirname(os.path.abspath(__file__))
 pr_root=os.path.dirname(bas_dir)
 model_path=os.path.join(pr_root,"model","rf_model.pkl")
-feat_path=os.path.join(pr_root,'model','feature,pkl')
+feat_path=os.path.join(pr_root,'model','feature.pkl')
 
 def preprocessing(data,feat):
     df=pd.DataFrame([data])
@@ -25,13 +25,16 @@ feat=joblib.load(feat_path)
 
 @app.route('/')
 def home():
-    print("Medical Cost Prediction API running! ")
+    return "Medical Cost Prediction API running! "
 
 @app.route("/predict",methods=['POST'])
 def predict():
-    data=request.json()
+    data=request.get_json()
 
     input_df=preprocessing(data,feat)
     prediction=model.predict(input_df)
 
-    return( jsonify({"Prediction": float(predict[0])}))
+    return( jsonify({"Prediction": float(prediction[0])}))
+
+if __name__=="__main__":
+    app.run(debug=True)
